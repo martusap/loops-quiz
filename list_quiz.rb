2165a1b63130cf930469037c72445a1ad67de8eb
April Matus <apmartus8@gmail.com>
2019-10-08 09:54:52 -0600

    bigger two

--- list_quiz.rb
+++ list_quiz.rb
@@ -13,8 +13,27 @@ def three_even(list)
     return false
 end
 
-puts three_even([2, 1, 3, 5])
-puts three_even([2, 4, 12, 5])
-puts three_even([1, 4, 6, 4])
-puts three_even([])
+# puts three_even([2, 1, 3, 5])
+# puts three_even([2, 4, 12, 5])
+# puts three_even([2, 3, 4, 6])
+# puts three_even([1, 4, 6, 4])
+# puts three_even([])
+
+
+def bigger_two(list1, list2)
+    sum1 = list1[0] + list1[1]
+    sum2 = list2[0] + list2[1]
+    if sum1 > sum2
+        return list1
+    end
+    if sum2 > sum1
+        return list2
+    end
+    if sum1 == sum2
+        return list1
+    end
+end
+
+puts bigger_two([1, 2], [3, 4])
+puts bigger_two([1, 7], [4, 4])
 
