0f98f6469336c46db8d5c83642cd94b963489018
April Matus <apmartus8@gmail.com>
2019-10-08 10:24:03 -0600

    series up

--- list_quiz.rb
+++ list_quiz.rb
@@ -2,10 +2,12 @@ def three_even(list)
     i = 0
     (list.size).times do
         if list[i] % 2 == 0
-            if list[i+1] % 2 == 0
-                if list[i+2] % 2== 0
-                    return true
-                end
+            if list[i+1] % 2 != 0
+                return false
+            end
+            if list[i+2] % 2== 0
+                return true
+            
             end
         end
         i += 1
@@ -15,7 +17,7 @@ end
 
 # puts three_even([2, 1, 3, 5])
 # puts three_even([2, 4, 12, 5])
-# puts three_even([2, 3, 4, 6])
+# puts three_even([2, 1, 4, 6])
 # puts three_even([1, 4, 6, 4])
 # puts three_even([])
 
@@ -34,6 +36,22 @@ def bigger_two(list1, list2)
     end
 end
 
-puts bigger_two([1, 2], [3, 4])
-puts bigger_two([1, 7], [4, 4])
+# puts bigger_two([1, 2], [3, 4])
+# puts bigger_two([1, 7], [4, 4])
 
+
+def series_up(num)
+    total = num*(num + 1) / 2
+    times = 1
+    list = []
+    num.times do
+        list = [list, list, times]
+        times += 1
+    end
+    puts list
+end
+
+puts series_up(1)
+puts series_up(2)
+puts series_up(3)
+puts series_up(4)
